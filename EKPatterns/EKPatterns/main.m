a488172ca8f1c70215745f510a3b116c83cbe0fe
EvgenyKarkan <evgenykarkan@gmail.com>
2013-11-18 23:44:37 +0200

    Project added.

---
+++ EKPatterns/EKPatterns/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  EKPatterns
+//
+//  Created by Evgeny Karkan on 18.11.13.
+//  Copyright (c) 2013 EvgenyKarkan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
